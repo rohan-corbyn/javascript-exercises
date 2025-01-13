357e8a0cbc2520e3d229822468e9cd38f334d93a
rohancorbyn <17966183+rohan-corbyn@users.noreply.github.com>
2025-01-13 16:40:39 +0000

    Added features to plop file so that new chapters can be created.
    - When providing an unused chapter name a new
        chapter will be created.
    - When providing an exercise name that's already
        in use by an exercise other than the most
        recent you will also now see an error message.

--- generators/helpers.js
+++ generators/helpers.js
@@ -1,4 +1,62 @@
-const { readdir } = require("fs/promises");
+const { readdir, mkdir } = require("fs/promises");
+
+function prependZero(number) {
+  console.log(number)
+  if (number < 9)
+    return "0" + number;
+  else
+    return number;
+}
+
+async function getLatestDirectoryNumber(chapterName) {
+  try {
+    const path = chapterName ? "./" + chapterName + "/" : "./";
+    const files = await readdir(path);
+    let latestDirectory = files.findLast((file) => /^\d+_\w+$/.test(file));
+    const latestDirectoryNumber = !latestDirectory ? 0 : parseInt(latestDirectory.match(/^\d+/));
+    console.log(latestDirectoryNumber)
+    return latestDirectoryNumber;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getExistingDirectory(directory, chapterName) {
+  try {
+    const path = chapterName ? "./" + chapterName + "/" : "./";
+    const files = await readdir(path);
+
+    return files.findLast((file) => new RegExp(`^\\d+_${directory}$`).test(file));
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function createExerciseDirectoryName(directoryName, chapterName) {
+
+  const existingExerciseDirectory = await getExistingDirectory(directoryName, chapterName)
+  if (existingExerciseDirectory) {
+    throw new Error(`Exercise already exists in ./${"${existingExerciseDirectory}"}/ with name "${existingExerciseDirectory}"`);
+  }
+  else {
+    const latestExerciseNumber = await getLatestDirectoryNumber(chapterName);
+    let returnval = `${prependZero(latestExerciseNumber + 1)}_${directoryName}`;
+    return returnval;
+  }
+}
+
+async function createChapterDirectoryName(directoryName) {
+  const existingChapterDirectory = await getExistingDirectory(directoryName)
+
+  if (existingChapterDirectory) {
+    return existingChapterDirectory;
+  } else {
+    const latestChapterNumber = await getLatestDirectoryNumber();
+    const path = `./${prependZero(latestChapterNumber + 1)}_${directoryName}`;
+    await mkdir(path);
+    return path;
+  }
+}
 
 function splitDirectoryName(directoryName) {
   return {
@@ -7,24 +65,4 @@ function splitDirectoryName(directoryName) {
   };
 }
 
-async function getLatestExerciseDirectory() {
-  try {
-    const files = await readdir("./");
-    return files.findLast((file) => /^\d+_\w+$/.test(file));
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-async function createExerciseDirectoryName(directoryName) {
-  const latestExerciseDirectory = await getLatestExerciseDirectory();
-  const latestExerciseNumber = parseInt(latestExerciseDirectory.match(/^\d+/));
-
-  if (latestExerciseDirectory === `${latestExerciseNumber}_${directoryName}`) {
-    throw new Error(`Exercise already exists with name "${directoryName}"`);
-  }
-
-  return `${latestExerciseNumber + 1}_${directoryName}`;
-}
-
-module.exports = { createExerciseDirectoryName, splitDirectoryName };
+module.exports = { createChapterDirectoryName, createExerciseDirectoryName, splitDirectoryName };
--- plopFile.js
+++ plopFile.js
@@ -1,44 +1,66 @@
 const { mkdir } = require("fs/promises");
 const { join } = require("path");
-const { camelCase } = require("case-anything");
-const { createExerciseDirectoryName } = require("./generators/helpers");
+const { camelCase, pascalCase } = require("case-anything");
+const { createChapterDirectoryName, createExerciseDirectoryName } = require("./generators/helpers");
 const { writeReadme } = require("./generators/writeReadme");
 const { writeExercise } = require("./generators/writeExercise");
 const { writeExerciseSpec } = require("./generators/writeExerciseSpec");
 
 module.exports = function (plop) {
   plop.setActionType("createExercise", async function (answers) {
-    const { exerciseName } = answers;
+
+    // ------------------------ //
+    let { chapterName, exerciseName } = answers;
+
+    if (!chapterName) {
+      throw new Error(
+        `Invalid exerciseChapterName. Expected: valid string. Actual: "${chapterName}"`
+      );
+    }
+
     if (!exerciseName) {
       throw new Error(
         `Invalid exerciseName. Expected: valid string. Actual: "${exerciseName}"`
       );
     }
+    //format
+    // ---------------- //
 
-    const camelExerciseName = camelCase(exerciseName);
-    const exerciseDirectoryName = await createExerciseDirectoryName(
-      camelExerciseName
-    );
-    const basePath = join("./", exerciseDirectoryName);
+    const formattedChapterName = await createChapterDirectoryName(
+      pascalCase(chapterName));
+
+    const formattedExerciseName = await createExerciseDirectoryName(
+      camelCase(exerciseName), formattedChapterName);
+
+    const basePath = join(`./${formattedChapterName}/`, formattedExerciseName);
     const solutionPath = join(basePath, "solution");
+
+    //write
+    // ---------------- //
 
     await mkdir(basePath);
     await mkdir(solutionPath);
-
     await writeReadme(basePath);
     await writeExercise(basePath);
     await writeExercise(solutionPath);
     await writeExerciseSpec(basePath);
     await writeExerciseSpec(solutionPath);
+
+    // ------------------------ //
   });
 
   plop.setGenerator("Basic", {
-    description: "Create a basic JavaScript exercise.",
+    description: "Create a basic JavaScript Exercise.",
     prompts: [
       {
         type: "input",
+        name: "chapterName",
+        message: "What is the name of the Chapter? (PascalCase)",
+      },
+      {
+        type: "input",
         name: "exerciseName",
-        message: "What is the name of the exercise? (camelCase)",
+        message: "What is the name of the Exercise? (camelCase)",
       },
     ],
     actions: [{ type: "createExercise" }],
